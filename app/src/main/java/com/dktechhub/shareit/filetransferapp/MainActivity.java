8cb64f16e5d4fd9ea9dbf3ba1002f0574a47c3fd
durgeshk <kdurgesh029@gmail.com>
2022-05-31 17:20:59 +0530

    File permission fixed for all

--- app/src/main/java/com/dktechhub/shareit/filetransferapp/MainActivity.java
+++ app/src/main/java/com/dktechhub/shareit/filetransferapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.dktechhub.shareit.filetransferapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -22,15 +26,30 @@ public class MainActivity extends AppCompatActivity {
         alertDialog = builder.create();
     }
     void mm1(boolean b)
-    {   if(isLanConnected())
-        startActivity(new Intent(MainActivity.this,ShareActivity.class).putExtra("receiver",b));
-        else {
+    {   if(!canWrite())
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+            }
+        }else if(!isLanConnected())
+        {
         alertDialog.show();
-        }
+        }else
+        startActivity(new Intent(MainActivity.this,ShareActivity.class).putExtra("receiver",b));
+
     }
 
     boolean isLanConnected()
     {
         return MWebHotspot.getInstance(this).getLocalIP().length()>0;
+    }
+    boolean canWrite()
+    {
+        return !(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M&&Build.VERSION.SDK_INT<Build.VERSION_CODES.Q&&checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
